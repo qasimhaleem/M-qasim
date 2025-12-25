6abac95618a9c4e37805240dcc576799df1ab118
Qasim Haleem <mrqasimhaleem@gmail.com>
2025-12-25 05:26:08 -0800

    this website is not done yet but i will it by latter

---
+++ things with div/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center bg-red-700 h-screen  '>
+      <p className=''>red</p>
+      <p>green</p>
+      <p>blue</p>
+    </div>
+  )
+}
+export default App
